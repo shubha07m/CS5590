b582b2f6c2d9492d900753d7c0e0739bcf2e14a2
Shubhabrata Mukherjee <shubha07m@gmail.com>
2020-05-12 21:14:50 -0500

    Updated version

--- dl1_prob1.py
+++ dl1_prob1.py
@@ -14,10 +14,14 @@ X_train, X_test, Y_train, Y_test = train_test_split(dataset[:, 0:8], dataset[:, 
                                                     test_size=0.25, random_state=87)
 np.random.seed(155)
 my_first_nn = Sequential()  # create model
-my_first_nn.add(Dense(30, input_dim=8, activation='relu'))  # hidden layer
+my_first_nn.add(Dense(50, input_dim=8, activation='relu'))  # hidden layer
+my_first_nn.add(Dense(20, activation='relu'))  # hidden layer
 my_first_nn.add(Dense(1, activation='sigmoid'))  # output layer
 my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
 my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100,
                                      initial_epoch=0)
 print(my_first_nn.summary())
-print(my_first_nn.evaluate(X_test, Y_test))
+acc = ((my_first_nn.evaluate(X_test, Y_test))[1])*100
+loss = ((my_first_nn.evaluate(X_test, Y_test))[0])
+print('accuracy of the model is: ' + str(acc)[:5] + '%')
+print('loss of the model is: ' + str(loss)[:5])
--- dl1_prob_23.py
+++ dl1_prob_23.py
@@ -13,18 +13,23 @@ dataset = pd.read_csv("breastcancer.csv")
 le = LabelEncoder()
 sc = StandardScaler()
 
-dataset.diagnosis = le.fit_transform(dataset.diagnosis)
-dataset = dataset.values
-dataset = sc.fit_transform(dataset)
+target = (np.array(dataset))[:, -1]
+target = le.fit_transform(target)
+feature = (np.array(dataset))[:, :-1]
+sc = sc.fit_transform(feature)
+X_train, X_test, Y_train, Y_test = train_test_split(feature, target, test_size=0.25, random_state=87)
 
-X_train, X_test, Y_train, Y_test = train_test_split(dataset[:, 0:-1], dataset[:, -1],
-                                                    test_size=0.25, random_state=87)
 np.random.seed(155)
 my_first_nn = Sequential()  # create model
-my_first_nn.add(Dense(30, input_dim=31, activation='relu'))  # hidden layer
+my_first_nn.add(Dense(50, input_dim=31, activation='relu'))  # hidden layer
+my_first_nn.add(Dense(20, activation='relu'))  # hidden layer
 my_first_nn.add(Dense(1, activation='sigmoid'))  # output layer
 my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
 my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100,
                                      initial_epoch=0)
+
 print(my_first_nn.summary())
-print(my_first_nn.evaluate(X_test, Y_test))
+acc = ((my_first_nn.evaluate(X_test, Y_test))[1])*100
+loss = ((my_first_nn.evaluate(X_test, Y_test))[0])
+print('accuracy of the model is: ' + str(acc)[:5] + '%')
+print('loss of the model is: ' + str(loss)[:5])
